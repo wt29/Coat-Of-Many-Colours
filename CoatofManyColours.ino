3408f203d8b2494be1ab86ff22d511134edbf71c
wt29 <tglynn@hotmail.com>
2023-04-05 12:57:30 +1000

    Pride Special
    Pride Special

--- CoatofManyColours.ino
+++ CoatofManyColours.ino
@@ -6,18 +6,32 @@
  *  
  *  Hit the reset button on the board as you hit Upload. The board goes into 
  *  Bootloader mode for a bit before it resumes the current program
- */
+ LEDs are numbered as follows
+
+ 8  9  26  27  44  45  62
+ 7 10  25  28  43  46  61
+ 6 11  24  29  42  47  60
+ 5 12  23  30  41  48  59
+ 4 13  22  31  40  49  58
+ 3 14  21  32  39  50  57
+ 2 15  20  33  38  51  56
+ 1 16  19  34  37  52  55
+ 0 17  18  35  36  53  54
+
+*/
 
 #include <FastLED.h>
 #define NUM_LEDS 63
 #define DATA_PIN 4
 
 int wait=10;   // millis()
-int waitClear=10; 
+int waitClear=10 ; 
+int intSize = sizeof( int );
+int hue;
 
 CRGB leds[NUM_LEDS];
 
-int x, e, o, c, z;
+//  int x, e, o, c, z;
 
 int Evens[] = {0,2,4,6,8,10,12,14,16};
 int Odds[] =  {1,3,5,7,9,11,13,15,17};
@@ -39,7 +53,7 @@ int letterL[] = {9,10,11,12,13,14,15,16,17,18,35,36,53};
 int letterM[] = {0,1,2,3,4,5,6,7,8,9,10,23,24,31,32,41,42,45,46,54,55,56,57,58,59,60,61,62};
 int letterN[] = {9,10,11,12,13,14,15,16,17,25,26,29,30,31,38,39,40,52,53,54,55,56,57,58,59,60,61,62};
 int letterO[] = {10,11,12,13,14,15,16,18,26,27,35,36,44,46,47,48,48,50,51,52};
-int letterP[] = {9,10,11,12,13,14,15,16,17,26,27,44,46,60,59,49,39,32,21 };
+int letterP[19] = {9,10,11,12,13,14,15,16,17,26,27,44,46,60,59,49,39,32,21 };
 int letterR[] = {0,1,2,3,4,5,6,7,8,9,13,26,22,27,31,32,38,40,44,46,49,52,54,58,59,60};
 int letterS[] = {19,13,14,15,16,17,18,22,26,27,31,35,36,40,44,45,46,47,48,49,53};
 int letterT[] = {8,9,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,44,45,62};
@@ -47,77 +61,41 @@ int letterT[] = {8,9,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,44,45
 int rows[] = {0,17,18,35,36,53,54};
 int centre[] = {31};
 int c1[] = {23, 30, 41, 40, 39, 32, 21, 22 };
-int c2[] = {11, 24,29,42,47,48,49,50,51,38,33,39,20,15,14,13,12};
-int c3[] = {7, 10,25,28,43,46,61,60,59,58,57,56,55,52,37,34,19,16,1,2,3,4,5,6};
+int c2[] = {11,24,29,42,47,48,49,50,51,38,33,39,20,15,14,13,12};
+int c3[] = {7,10,25,28,43,46,61,60,59,58,57,56,55,52,37,34,19,16,1,2,3,4,5,6};
+
 int topBar[] = {8,9,26,27,44,45,62};
 int botBar[] = {0,17,18,35,36,53,54};
+int spiral[] = {31,30,23,22,21,32,39,40,41,42,29,24,11,12,13,14,15,20,33,38,51,50,49,48,47,46,43,28,25,10,7,6,5,4,3,2,1,16,19,34,37,52,55,56,57,58,59,60,61,62,45,44,27,26,9,8,0,17,18,35,36,53,54};
+
 #define NUM_COLORS 6
-CRGB rainbow[NUM_COLORS]  = { CRGB::Red, CRGB::Orange, CRGB::Yellow, CRGB::Green, CRGB::Blue, CRGB::Violet };
+CRGB rainbow[]  = { CRGB::Red, CRGB::Orange, CRGB::Yellow, CRGB::Green, CRGB::Blue, CRGB::Violet };
 
 void setup() {
   
 FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
-
 Serial.begin(115200);
 
 clearAll(waitClear);  
-
 }
 
 void loop() {
 
-int rainBow, c_centre, c_c1, c_c2, c_c3;
-clearAll( 50 );
-for (int x=0; x <= 3 ; x++ ){
- for ( rainBow = 0; rainBow <= NUM_COLORS-1; rainBow++ ){
-  Serial.print( "rainBow " );
-  Serial.println( rainBow );
-  Serial.println(  x );
-  showLeds( c3, rainbow[c_c2], false, sizeof( c3 ) ); 
-  showLeds( c2, rainbow[c_c1], false, sizeof( c2 ) );
-  c_c2 = c_c1;
-  showLeds( c1, rainbow[c_centre], false, sizeof( c1 ) );
-  c_c1 = c_centre;
-  showLeds( centre, rainbow[rainBow], false, sizeof( centre ) );
-  showLeds( topBar, rainbow[rainBow], false, sizeof( topBar ) );
-  showLeds( botBar, rainbow[rainBow], false, sizeof( botBar ) );
-  c_centre = rainBow;
-  delay( 500 );
- }
-}
+ rainBowOut();
 
-clearAll( 50 );
-for (int x=0; x <= 3 ; x++ ){
- for ( rainBow = 0; rainBow <= NUM_COLORS-1; rainBow++ ){
-  Serial.print( "rainBow " );
-  Serial.println( rainBow );
-  Serial.println(  x );
-    showLeds( centre, rainbow[c_c1], false, sizeof( centre ));
-    showLeds( topBar, rainbow[c_c1], false, sizeof( topBar ));
-    showLeds( botBar, rainbow[c_c1], false, sizeof( botBar ));
-    showLeds( c1, rainbow[c_c2], false, sizeof( c1 ) );
-    c_c1 = c_c2;
-    showLeds( c2, rainbow[c_c3], false, sizeof( c2 ) );
-    c_c2 = c_c3;
-    showLeds( c3, rainbow[rainBow], false, sizeof( c3 ) ); 
-    c_c3 = rainBow;
-  delay( 500 );
- }
-}
+ pride();
 
+ rainBowIn();
 
-clearAll( 50 );
- for ( rainBow = 0; rainBow <= NUM_COLORS-1; rainBow++ ){
-   showLeds( letterP, rainbow[rainBow], true, sizeof( letterP ) );
-   showLeds( letterR, rainbow[rainBow], true, sizeof( letterR ) );
-   showLeds( letterI, rainbow[rainBow], true, sizeof( letterI ) );
-   showLeds( letterD, rainbow[rainBow], true, sizeof( letterD ) );
-   showLeds( letterE, rainbow[rainBow], true, sizeof( letterE ) );
-   delay( 1000 );
- }
+ pride();
 
-clearAll( 50 );
-cylon(7);
+ spiralShow();
+
+ pride();
+
+ cylon(7);
+ 
+ pride();
  
 }
 
@@ -130,35 +108,108 @@ void oneLed( CRGB colour, bool clear, int aLed ){
   }
 }
 
-void showLeds( int lArray[], CRGB colour, bool clear, int sizeOfArray  ){
+void showLeds( int lArray[], CRGB colour, int arrLen  ){
+ for (int x=0; x < arrLen ; x++ ){
+//  leds[ lArray[x] ] = colour;
+     leds[ lArray[x] ] = CHSV(hue++, 255, 255);
+ }
 
-for (int x=0; x < ( sizeOfArray/sizeof(int)) ; x++ ){
-  leds[ lArray[x] ] = colour; 
-    
-}
-  FastLED.show(); 
-  delay( wait );
-  if (clear){ 
-    clearAll( waitClear );
-  }
+ FastLED.show(); 
 }
 
 
 void clearAll(int clearWait ) {
-
-  for( c=0; c < NUM_LEDS ; c++ ) {
-    delay(5);
-    leds[c] = CRGB::Black;
-  }
-     FastLED.show();
-     delay( clearWait );  
-  }
-  
+ int c;
+ for( c=0; c < NUM_LEDS ; c++ ) {
+  delay(5);
+  leds[c] = CRGB::Black;
+ }
+ FastLED.show();
+ delay( clearWait );  
+}
 
 void fadeall() { for(int i = 0; i < NUM_LEDS; i++) { leds[i].nscale8(250); } }
 
+void rainBowOut() {
+ int rainBow, c_centre, c_c1, c_c2, c_c3, x;
+ clearAll(300);
+ for (int x=0; x <= 10 ; x++ ){
+//  for ( rainBow = 0; rainBow <= NUM_COLORS-1; rainBow++ ){
+   showLeds( c3, hue++, sizeof( c3 )/2 ); 
+   showLeds( c2, hue++, sizeof( c2 )/2 );
+   c_c2 = c_c1;
+   showLeds( c1, hue++, sizeof( c1 )/2 );
+   c_c1 = c_centre;
+   showLeds( centre, hue++, sizeof( centre )/2 );
+   showLeds( topBar, hue++, sizeof( topBar )/2 );
+   showLeds( botBar, hue++, sizeof( botBar )/2 );
+   c_centre = rainBow;
+   delay( 400 );
+//  }
+ }
+} 
+
+void rainBowIn() {
+ int rainBow, c_centre, c_c1, c_c2, c_c3, x;
+ clearAll(300);
+ for (int x=0; x <= 10 ; x++ ){
+//  for ( rainBow = 0; rainBow <= NUM_COLORS-1; rainBow++ ){
+   showLeds( centre, hue++, sizeof( centre )/2 );
+   showLeds( topBar, hue++, sizeof( topBar )/2 );
+   showLeds( botBar, hue++, sizeof( botBar )/2 );
+   showLeds( c1, hue++, sizeof( c1 )/2 );
+   c_c1 = c_c2;
+   showLeds( c2, hue++, sizeof( c2 )/2 );
+   c_c2 = c_c3;
+   showLeds( c3, hue++, sizeof( c3 )/2 ); 
+   c_c3 = rainBow;
+   delay( 400 );
+ // }
+ }
+}
+
+void pride() {
+ int colour;
+ // for ( colour = 0; colour <= NUM_COLORS-1; colour++ ){
+  clearAll(0);
+  showLeds( letterP, rainbow[colour], sizeof( letterP ) /2 );
+  delay(200);
+  clearAll(0);
+  showLeds( letterR, rainbow[colour], sizeof( letterR ) /2 );
+  delay(200);
+  clearAll(0);
+  showLeds( letterI, rainbow[colour], sizeof( letterI ) /2 );
+  delay(200);
+  clearAll(0);
+  showLeds( letterD, rainbow[colour], sizeof( letterD ) /2 );
+  delay(200);
+  clearAll(0);
+  showLeds( letterE, rainbow[colour], sizeof( letterE ) /2 );
+  delay(200);
+ // }
+
+}
+
+void spiralShow() {
+ int colour = 0, arrSize = sizeof( spiral )/intSize;
+ clearAll(0);
+ Serial.println( arrSize );
+ for ( int j=1; j < 10; j++ ) {
+  for ( int x=0; x < ( arrSize ) ; x++ ){
+   leds[ spiral[ x ] ] = CHSV(hue++, 255, 255);
+   FastLED.show();
+  //colour++;
+ // if (colour >= NUM_COLORS ) {
+  //  colour = 0;
+ //   };
+  delay( 20 );
+  }
+ }
+}
+
 void cylon(int repeats) { 
   static uint8_t hue = 0;
+  clearAll(0);
   // First slide the led in one direction
   for(int j = 1; j <= repeats; j++ ){
   for(int i = 0; i < NUM_LEDS; i++) {
@@ -187,7 +238,6 @@ void cylon(int repeats) {
   }
   }  // Do it a couple of times
 }
-
 
 /*
 // Tech Central 
